d982092cc3df90944afc4eb99be2e864ad0a3e19
nferreira1 <nathan.1402@hotmail.com>
2024-09-18 18:34:32 -0300

    :recycle: refactor: componentização dos produtos recomendados

--- frontend/src/categorias/index.js
+++ frontend/src/categorias/index.js
@@ -19,13 +19,13 @@ const handleFetchCategorias = async () => {
     if (!loading) {
         sectionCategorias.innerHTML = data.map(categoria => `
             <a href="/categorias/produtos?categoriaId=${categoria.id}">
-                <div class="w-40 h-48 flex flex-col rounded-md overflow-hidden cursor-pointer">
+                <div class="w-40 lg:w-96 h-48 lg:h-64 flex flex-col rounded-md overflow-hidden cursor-pointer">
                     <div class="grow flex items-center justify-center bg-gradient-to-tr from-primary to-primary/30">
-                        <img alt={categoria.nome} src=${categoria.imagem} class="object-contain size-32">
+                        <img alt={categoria.nome} src=${categoria.imagem} class="object-contain size-32 lg:size-40">
                     </div>
                 
-                    <div class="h-10 w-full bg-muted grid place-items-center">
-                        <span class="text-xs truncate font-semibold">${categoria.nome}</span>
+                    <div class="h-10 lg:min-h-14 w-full bg-muted grid place-items-center">
+                        <span class="text-xs lg:text-base truncate font-semibold">${categoria.nome}</span>
                     </div>
                 </div>
             </a>

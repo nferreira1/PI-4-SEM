7019214e9987b1ebd40099793b37ee8739103303
nferreira1 <nathan.1402@hotmail.com>
2024-10-23 09:03:39 -0300

    :sparkles: feat: criação de funções para validar o schema da página e para validar valores em reais, e também uso de openapi-fetch para efetuar requisições

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,8 +1,18 @@
-import createClient from "openapi-fetch";
+import createClient, { Middleware } from "openapi-fetch";
 import { paths } from "swagger/v1/api-docs";
 
 export const BASE_URL = process.env.API_URL;
 
+const middleware: Middleware = {
+	async onResponse({ response }) {
+		if (!response.ok) {
+			throw new Error(await response.text());
+		}
+	},
+};
+
 export const api = createClient<paths>({
 	baseUrl: BASE_URL,
 });
+
+api.use(middleware);

c3c60e481540ce3eaca9916c9df6db90cfb11d66
nferreira1 <nathan.1402@hotmail.com>
2024-11-03 02:28:54 -0300

    :sparkles: feat: inserção de nova configuração do model mapper, para sempre passar o e-mail como minúsculo

--- backend/src/main/java/br/edu/senac/config/ModelMapperConfig.java
+++ backend/src/main/java/br/edu/senac/config/ModelMapperConfig.java
@@ -25,9 +25,16 @@ public class ModelMapperConfig {
             return genero;
         };
 
+        Converter<String, String> emailToLowerConverter = context -> {
+            String email = context.getSource();
+            return email != null ? email.toLowerCase() : null;
+        };
+
         modelMapper.typeMap(ClienteDTO.class, ClienteEntity.class)
-                .addMappings(mapper -> mapper.using(generoConverter)
-                        .map(ClienteDTO::getGeneroId, ClienteEntity::setGeneroEntity));
+                .addMappings(mapper -> {
+                    mapper.using(generoConverter).map(ClienteDTO::getGeneroId, ClienteEntity::setGeneroEntity);
+                    mapper.using(emailToLowerConverter).map(ClienteDTO::getEmail, ClienteEntity::setEmail);
+                });
 
         return modelMapper;
     }

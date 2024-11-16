7b1e7a429589324c8f6d47075550f30165038b9c
nferreira1 <nathan.1402@hotmail.com>
2024-11-15 23:21:05 -0300

    :sparkles: feat: adição do componente alert-dialog da lib shadcn/ui e configuração do react-query

--- frontend/src/lib/api.ts
+++ frontend/src/lib/api.ts
@@ -1,18 +1,18 @@
-import createClient, { Middleware } from "openapi-fetch";
+import createFetchClient, { Middleware } from "openapi-fetch";
+import createClient from "openapi-react-query";
 import { paths } from "swagger/v1/api-docs";
 
-export const BASE_URL = process.env.API_URL;
+export const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const middleware: Middleware = {
-	async onResponse({ response }) {
-		if (!response.ok) {
-			throw new Error(await response.text());
-		}
-	},
+	async onResponse() {},
+	async onRequest() {},
 };
 
-export const api = createClient<paths>({
+export const api = createFetchClient<paths>({
 	baseUrl: BASE_URL,
 });
+
+export const $api = createClient<paths>(api);
 
 api.use(middleware);
